b1513fd0f5615f2406ffcb909c101370ee166ff2
Ragnar Joosep <ragnar.joosep@khk.ee>
2021-05-24 09:49:20 +0100

    busside arvutus

---
+++ praks3/yl4.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+#Autor- Ragnar Joosep
+# Busside arvutamise valem
+
+echo -n "Mitu inimest on grupis?:"
+read number
+echo -n "Mitu kohta on bussis?:"
+read arv
+
+var1=$(($number-$arv))
+echo $var1
