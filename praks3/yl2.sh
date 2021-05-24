5370452c8d351ac187dcc7450fb5c11fe0fa63e7
Ragnar Joosep <ragnar.joosep@khk.ee>
2021-05-24 09:39:49 +0100

    muutuja väärtus

---
+++ praks3/yl2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#autor- Ragnar Joosep
+# Muutuja  väärtus
+
+president="Kersti Kaljulaid"
+aasta=2016
+lause=". aastal valiti Eesti presidendiks."
+lause=". aastal valiti Eesti presidendiks "
+echo "$aasta$lause$president"
