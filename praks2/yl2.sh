e8a573971b73dee93f8f253867d5a3a8300d783c
Ragnar Joosep <ragnar.joosep@khk.ee>
2021-05-24 09:21:45 +0100

    kell ja kuupäev

---
+++ praks2/yl2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#autor- Ragnar Joosep
+# kuupäeva ja kellaaja väljastus
+now=$(date +"%T")
+echo "Hetkene kellaaeg: $now"
+echo "Tere $USER"
+echo "Hetkene kellaaeg: $now"
+echo "Siin on t2nane kuup2ev"
+cal
